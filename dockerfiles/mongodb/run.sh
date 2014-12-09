a0df9bdb302d4d818409604a8cf97b745406a662
kerkerj <jerry54010@gmail.com>
2014-12-09 18:01:13 +0800

    modify mongodb dir

--- dockerfiles/mongodb/run.sh
+++ dockerfiles/mongodb/run.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -p 27017:27017 -p 27018:27018 -v /data/db:/data/db --name mongodb dockerfile/mongodb
+sudo docker run -d -p 27017:27017 -p 27018:27018 -v /data/mongodb:/data/db --name mongodb dockerfile/mongodb
