9abc8495278265bcd728df127e30e8dc79fd8c53
kerkerj <jerry54010@gmail.com>
2014-12-09 18:27:13 +0800

    +x to script

--- dockerfiles/mongodb/run.sh
+++ dockerfiles/mongodb/run.sh
@@ -1,1 +1,3 @@
 sudo docker run -d -p 27017:27017 -p 27018:27018 -v /data/mongodb:/data/db --name mongodb mongodb:latest
+
+sudo docker run -d -p 27017:27017 -p 27018:27018 -v /data/mongodb:/data/db --name mongodb mongodb:latest
