c8d72e88769631a76fa806ad7371fcca6817b868
kerkerj <jerry54010@gmail.com>
2014-12-15 08:36:58 +0000

    add test project, modify scripts and dockerfiles

--- dockerfiles/Nginx/run.sh
+++ dockerfiles/Nginx/run.sh
@@ -8,4 +8,24 @@ read -p "Choose image name to run Nginx: " image
 printf "\n"
 printf "Running: sudo docker run -it $image \n"
 
+printf "1. sudo docker run -it -p 80:80 -d $image \n"
+printf "2. sudo docker run -it -p 80:80 -p 3000:3000 -d --link nodejsapp:nodejsapp -v /var/log/nginx:/var/log/nginx -v /home/ubuntu/DockerVagrant/dockerfiles/Nginx/conf.d/:/etc/nginx/conf.d $image \n\n"
+
+read -p "Choose an action: " number
+
+case $number in 
+
+1)
 sudo docker run -it -p 80:80 -d $image 
+;;
+
+2)
+sudo docker run -it -p 80:80 -p 3000:3000 -d --link nodejsapp:nodejsapp -v /var/log/nginx:/var/log/nginx -v /home/ubuntu/DockerVagrant/dockerfiles/Nginx/conf.d/:/etc/nginx/conf.d $image
+;;
+
+*)
+printf "Wrong ans"
+;;
+
+esac
+
